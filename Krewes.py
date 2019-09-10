89ddae02826b8d8393c6ba5781c1a0cee289f3e7
TiffanyCao <43053349+TiffanyCao@users.noreply.github.com>
2019-09-10 13:58:27 -0400

    added comments

--- Krewes.py
+++ Krewes.py
@@ -5,8 +5,10 @@ KREWES = {
     'rex': ['William', 'Joseph', 'Calvin', 'Ethan', 'Moody', 'Mo', 'Big Mo', 'Peihua', 'Saad',],
     'endymion': ['Grace', 'Nahi', 'Derek', 'Jun Tao', 'Connor', 'Jason', 'Tammy', 'Albert',]}
 
+# Retrieve a random team name 
 kes = random.choice(list(KREWES.keys()))
 
-
 # print kes
+
+# Retrieve a random name from the random team given by kes
 print KREWES[kes][random.randint(0, len(KREWES[kes]) - 1)]
