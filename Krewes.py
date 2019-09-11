a8c574579d0fdbce2482023cc523211e0556fc09
Tiffany Cao <tcao00@stuy.edu>
2019-09-10 20:07:46 -0400

    rewrote function to have parameter team name

--- Krewes.py
+++ Krewes.py
@@ -5,10 +5,16 @@ KREWES = {
     'rex': ['William', 'Joseph', 'Calvin', 'Ethan', 'Moody', 'Mo', 'Big Mo', 'Peihua', 'Saad',],
     'endymion': ['Grace', 'Nahi', 'Derek', 'Jun Tao', 'Connor', 'Jason', 'Tammy', 'Albert',]}
 
-# Retrieve a random team name 
-kes = random.choice(list(KREWES.keys()))
-
-# print kes
+# Retrieve a random team name
+# kes = random.choice(list(KREWES.keys()))
 
 # Retrieve a random name from the random team given by kes
-print KREWES[kes][random.randint(0, len(KREWES[kes]) - 1)]
+# print KREWES[kes][random.randint(0, len(KREWES[kes]) - 1)]
+
+# Given team name, retrieve a random name from that team
+def getName(team):
+    print KREWES[team][random.randint(0, len(KREWES[team]) - 1)]
+
+getName('orpheus')
+getName('rex')
+getName('endymion')
