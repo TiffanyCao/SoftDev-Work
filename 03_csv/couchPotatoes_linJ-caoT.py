eb92d63a22a81fb663ee4c7999c5b8fcc7880df1
Tiffany Cao <tcao00@stuy.edu>
2019-09-13 22:23:29 -0400

    code extracts occupation and percentage separately with splicing

--- 03_csv/couchPotatoes_linJ-caoT.py
+++ 03_csv/couchPotatoes_linJ-caoT.py
@@ -1,4 +1,4 @@
-
+import random
 
 #create dictionary based on csv
 dict = {} #initialize a new dictionary structure
@@ -6,5 +6,13 @@ file = open("occupations.csv", "r") #open up csv file for reading purposes
 content = file.readlines() #parse through file by lines
 content = content[1:len(content) - 1] #take out the first and last line
 for line in content:
-    print(line)
+    line = line.strip() #removes \n
+    line = line.lstrip('\"') #removes leading quote
+    if ("\"" in line): #if line contains quotation marks
+        line = line.split("\",") #splits line by ",
+    else:
+        line = line.split(",") #if line does not contain quotes, split by comma
+
+    dict[line[0]] = float(line[1]) #key value pair
+#print(dict) #testing results
 file.close()
