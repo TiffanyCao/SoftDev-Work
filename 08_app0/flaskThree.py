5967c45a57f5f527206e51f4a20b6f8a7b38989b
Tiffany Cao <tcao00@stuy.edu>
2019-09-18 13:28:32 -0400

    added second route

--- 08_app0/flaskThree.py
+++ 08_app0/flaskThree.py
@@ -6,6 +6,11 @@ def hello_world():
     print(__name__) #where will this go?
     return "No hablo queso!"
 
+@app.route("/two") #url
+def world2():
+    print(__name__)
+    return "Second route? I sure hope it is."
+
 if __name__ == "__main__":
     app.debug = True
     app.run()
