5c616a9939a9dc191ed96fd2ddcdeca510a65206
Tiffany Cao <tcao00@stuy.edu>
2019-09-18 14:57:40 -0400

    wrote and tested third route

--- 08_app0/flaskThree.py
+++ 08_app0/flaskThree.py
@@ -11,6 +11,11 @@ def world2():
     print(__name__)
     return "Second route? I sure hope it is."
 
+@app.route("/three") #url
+def world3():
+    print(__name__)
+    return "It's the third parallel universe! How do I get back?"
+
 if __name__ == "__main__":
     app.debug = True
     app.run()
