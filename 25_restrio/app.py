242386fa41c86945ec72dace9343035c30ef2c49
Tiffany Cao <tcao00@stuy.edu>
2019-11-13 12:24:58 -0500

    added different routes and home.html, fixed formatting for address autocomplete

--- 25_restrio/app.py
+++ 25_restrio/app.py
@@ -10,13 +10,18 @@ app = Flask(__name__)
 
 @app.route('/')
 def root():
+    return render_template("home.html")
+
+@app.route('/addressautocomplete')
+def address():
     u = urllib.request.urlopen(
     "https://us-autocomplete.api.smartystreets.com/suggest?auth-id=2676fc89-0bed-fde9-0ff6-b70829031c6c&auth-token=022h0rkvcmLqMZ7EXEii&prefix=4770%20Lincoln%20Ave%20O"
     )
     response = u.read()
     data = json.loads(response)
+    data = data['suggestions']
     # stuff = stuff['href']
-    return render_template("index.html", )
+    return render_template("index1.html", stuff = data, length = len(data))
 
 
 if __name__ == "__main__":
