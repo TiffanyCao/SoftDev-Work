611249ecc7dfe74edfadac6d4d9fd0e58f9e580b
Tiffany Cao <tcao00@stuy.edu>
2019-11-13 12:31:03 -0500

    added Jackie's API

--- 25_restrio/app.py
+++ 25_restrio/app.py
@@ -1,7 +1,7 @@
-#Tiffany Cao
+#Tiffany Cao & Jackie Lin
 #SoftDev1 pd1
-#K
-#2019
+#K25 -- Getting More REST
+#2019-11-13
 
 from flask import Flask, render_template
 import json
@@ -10,7 +10,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def root():
-    return render_template("home.html")
+    return render_template("main.html")
 
 @app.route('/addressautocomplete')
 def address():
@@ -22,6 +22,14 @@ def address():
     data = data['suggestions']
     # stuff = stuff['href']
     return render_template("index1.html", stuff = data, length = len(data))
+
+@app.route("/darksky")
+def darksky():
+    url = "https://api.darksky.net/forecast/28b607f4a70a742bd83b0931b1590466/42.3601,-71.0589"
+    u = urllib.request.urlopen(url)
+    response = u.read()
+    data = json.loads(response)
+    return render_template("darksky.html",lat=data['latitude'], long=data['longitude'], timezone=data['timezone'], time=data['currently']['time'], sum=data['currently']['summary'])
 
 
 if __name__ == "__main__":
