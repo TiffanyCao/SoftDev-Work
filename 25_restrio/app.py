60f9fb471ce7ca398dc22a67bee9365534b8663d
Tiffany Cao <tcao00@stuy.edu>
2019-11-13 19:13:27 -0500

    added second API called AirVisual

--- 25_restrio/app.py
+++ 25_restrio/app.py
@@ -31,6 +31,14 @@ def darksky():
     data = json.loads(response)
     return render_template("darksky.html",lat=data['latitude'], long=data['longitude'], timezone=data['timezone'], time=data['currently']['time'], sum=data['currently']['summary'])
 
+@app.route("/airquality")
+def air():
+    url = "http://api.airvisual.com/v2/city?city=Los%20Angeles&state=California&country=USA&key=e6ce6055-e9ac-4bf4-add0-3cff6f47a5ec"
+    u = urllib.request.urlopen(url)
+    response = u.read()
+    data = json.loads(response)
+    return render_template("airquality.html", city = data['data']['city'], state = data['data']['state'], country = data['data']['country'], poll = data['data']['current'], length = len(data['data']['current']))
+
 
 if __name__ == "__main__":
     app.debug = True
