a958403b2a265462b6966267646304514a1a9a5c
Tiffany Cao <tcao00@stuy.edu>
2020-02-05 10:13:22 -0500

    added heading

---
+++ clickdraw.js
@@ -0,0 +1,40 @@
+// The Cows -- Tiffany Cao and William Cao
+// SoftDev1 pd1
+// K04 -- I See a Red Door...
+// 2020-02-05
+
+var c = document.getElementById("slate")
+var ctx = c.getContext("2d");
+var mode = "rectangle"; // "rectangle" or "dot"
+var modeDisplay = document.getElementById("modeDisplay");
+var drawingInstructions = [];
+
+document.getElementById("clear").addEventListener("click", () => {
+  ctx.fillStyle = "#fff";
+  ctx.fillRect(0, 0, c.width, c.height);
+});
+
+document.getElementById("switch").addEventListener("click", () => {
+  if(mode === "rectangle"){
+    mode = "dot";
+  }else{
+    mode = "rectangle";
+  }
+  modeDisplay.innerHTML = mode;
+});
+
+c.addEventListener("click", (e) => {
+  var x = e.pageX - c.offsetLeft;
+  var y = e.pageY - c.offsetTop;
+  // console.log('clicked');
+  if(mode === "rectangle"){
+    ctx.fillStyle = "red";
+    ctx.fillRect(x, y, 50, 100);
+  }else{
+    ctx.beginPath();
+    ctx.arc(x, y, 5, 0, 2 * Math.PI);
+    ctx.stroke();
+    ctx.fillStyle = "blue";
+    ctx.fill();
+  }
+})
