e50798678483a96bfb8edba5b10ce033a5a76e37
Tiffany Cao <tcao00@stuy.edu>
2020-02-27 17:32:09 +0000

    added rest of the bullet points

--- 09_mongo/restaurants.py
+++ 09_mongo/restaurants.py
@@ -11,24 +11,24 @@ db = client.restaurants
 
 db.catalog.drop()  #clear everything in the collection first
 catalog = db.catalog
-with open("primer-dataset.json", "r") as file:
+with open("primer-dataset.json", "r") as file:  #read in json data
   content = file.readlines()
   for line in content:
     catalog.insert_one(loads(line))
 
 def findBorough(borough):
-  '''All restaurants in a specified borough'''
+  '''All restaurants in a specified borough.'''
   results = catalog.find({ "borough": borough })
   print("Borough: {}".format(borough))
   print("Results Found: {}".format(results.count()))
-  print() 
+  print()
   for x in results:
     print(x["name"])
 
 #findBorough("Queens")
 
 def findZip(zip):
-  '''All restaurants in a specified zipcode'''
+  '''All restaurants in a specified zipcode.'''
   results = catalog.find({ "address.zipcode": zip })
   print("Zipcode: {}".format(zip))
   print("Results Found: {}".format(results.count()))
@@ -36,4 +36,26 @@ def findZip(zip):
   for x in results:
     print(x["name"])
 
-findZip("11355")
+#findZip("11355")
+
+def findGrade(zip, grade):
+  '''All restaurants in a specified zipcode and with a specified grade.'''
+  results = catalog.find({ "address.zipcode": zip, "grades.grade": grade })
+  print("Zipcode: {}  Grade: {}".format(zip, grade))
+  print("Results Found: {}".format(results.count()))
+  print()
+  for x in results:
+    print(x["name"])
+
+#findGrade("11374", "A")
+
+def findScore(zip, score):
+  '''All restaurants in a specified zip code with a score below a specified threshold.'''
+  results = catalog.find({ "address.zipcode": zip, "grades.score": { "$lt": score }})
+  print("Zipcode: {}  Score: {}".format(zip, score))
+  print("Results Found: {}".format(results.count()))
+  print()
+  for x in results:
+    print(x["name"])
+
+#findScore("10019", 4)
