5e1afc7061377b0802da57a9eb8e7818e3a918f8
Tiffany Cao <tcao00@stuy.edu>
2020-03-14 23:42:04 -0400

    added html pages

--- 11_mongoflask/app.py
+++ 11_mongoflask/app.py
@@ -2,7 +2,7 @@
 #K10 -- Import/Export Bank
 #K11 --
 #2020-03-05
-from flask import Flask, render_template, request,session
+from flask import Flask, render_template, request, session, redirect
 # from pymongo import MongoClient
 import json
 
@@ -16,10 +16,23 @@ app = Flask(__name__)
 
 @app.route("/", methods=['GET'])
 def form():
-    return render_template("home.html")
+    check = request.args.get('search')
+    print(check)
+    if check is None:
+        return render_template("home.html", tags = False)
+    else:
+        return render_template("home.html", tags = True)
 
+# @app.route("/search", methods=['GET'])
+# def search():
+#     return render_template("home.html", tags = True)
 
 @app.route("/results", methods=['GET'])
+def start():
+    if request.args.get('ova') or request.args.get('movie') or request.args.get('special'):
+        return render_template("results.html")
+    else:
+        return redirect('error')
 def finished():
     li=[]
     for x in anime.find({"status": "FINISHED"}):
@@ -44,6 +57,10 @@ def gallery():
         all.append(x["title"]) #easier way??
         pic.append(x["picture"])
     return render_template("gallery.html",titles=all,pic=pic)
+
+@app.route("/error", methods=['GET'])
+def error():
+    return render_template("error.html")
 
 if __name__ == "__main__":
     app.debug = True
