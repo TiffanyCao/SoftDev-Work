52acc9a8f98615e5d25370230fe12387d6906489
Tiffany Cao <tcao00@stuy.edu>
2020-03-16 19:13:10 +0000

    tested anime.py

--- 11_mongoflask/anime.py
+++ 11_mongoflask/anime.py
@@ -1,61 +1,71 @@
 #Amanda Zheng, Tiffany Cao, Team Blank
-#K10 -- Import/Export Bank
 #K11 --
 #2020-03-05
+
 from flask import Flask, render_template, request,session
 from pymongo import MongoClient
 import json
 from bson.json_util import loads
 
 app = Flask(__name__)
+client = MongoClient("localhost", 27017)
+anime = client.weeb.anime
 
 def create():
-    client = MongoClient("localhost", 27017)
-    anime = client.weeb.anime
     anime.drop()
     file = open("anime.json", "r")
     doc = file.readlines()
     for x in doc:
         anime.insert_one(loads(x))
 
+#create()
+
 def findStatus(stat):
+    #print("status: " + stat)
+    #print(anime.find({ "status": stat }).count())
     return anime.find({ "status": stat })
 
 def findTitle(name):
     name=".*"+name+".*"
+    #print(name)
+    #print(anime.find({ "title": { "$regex": name, "$options": "i" }}).count())
     return anime.find({ "title": { "$regex": name, "$options" : "i" }})
 
 def findEp(num):
+    #print("lte " + str(num))
+    #print(anime.find({ "episodes": { "$lte": num }}).count())
     return anime.find({ "episodes": { "$lte": num }})
 
 def findType(type):
+    #print(type)
+    #print(anime.find({ "type": type }).count())
     return anime.find({ "type": type })
 
-for result in findStatus("CURRENTLY"):
-    if (result["title"]==""):
-        print("No Name Found")
-    else:
-      print (result["title"])
+#for result in findStatus("CURRENTLY"):
+    #if (result["title"]==""):
+      #print("No Name Found")
+    #else:
+      #print (result["title"])
 
-for result in findTitle("girl"):
-    if (result["title"]==""):
-        print("No Name Found")
-    else:
-      print (result["title"])
+#for result in findTitle("girl"):
+    #if (result["title"]==""):
+      #print("No Name Found")
+    #else:
+      #print (result["title"])
 
-for result in findEp(20):
-    if (result["title"]==""):
-        print("No Name Found")
-    else:
-      print (result["title"])
+#for result in findEp(20):
+    #if (result["title"]==""):
+      #print("No Name Found")
+    #else:
+      #print (result["title"])
 
-for result in findType("OVA"):
-    if (result["title"]==""):
-        print("No Name Found")
-    else:
-      print (result["title"])
+#for result in findType("OVA"):
+    #if (result["title"]==""):
+      #print("No Name Found")
+    #else:
+      #print (result["title"])
 
 
 if __name__ == "__main__":
     app.debug = True
-    app.run()
+    #app.run()
